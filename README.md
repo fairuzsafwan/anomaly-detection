af265ec45829823354fffbe59031061ee5f7e6c6
stanshi <fsafwan88@gmail.com>
2021-12-29 00:16:46 +0900

    updated

--- README.md
+++ README.md
@@ -7,16 +7,6 @@ Description:
 Detect anomalies via unsupervised method
 
 How to run:
-- d / --dataset : path to calibration dataset (create dataset by taking pictures of calibration board using camera)
-- i / --image    : image to remove distortion
-- t / --type     : Type of board, 0 = chessboard | 1 = aruco | 2 = charuco 
 ```
-$ python calibrate.py -d path/dataset -i imageToUndistort.jpg -t 0
+$ python anomaly.py
 ```
-
-
-To Do:
-------------------------
-  - [ ] combine aruco, charuco and chessboard all in one file
-  - [ ] Convert code to C++
-  - [ ] remove need for svgfig dependency in create_chessboard.py
--- anomaly.py
+++ anomaly.py
@@ -1,1 +1,24 @@
-import numpy as np
+from pycaret.anomaly import *
+from sklearn.datasets import load_breast_cancer
+
+df = load_breast_cancer(as_frame=True)['data']
+df_train = df.iloc[:-10]
+df_unseen = df.tail(10)
+
+anom = setup(data = df_train, silent = True)
+
+anom_model = create_model(model = 'iforest', fraction = 0.05)
+
+results = assign_model(anom_model)
+
+plot_model(anom_model, plot = 'tsne')
+
+plot_model(anom_model, plot = 'umap')
+
+save_model(model = anom_model, model_name = 'iforest_model')
+
+loaded_model = load_model('iforest_model')
+
+loaded_model.predict(df_unseen)
+loaded_model.predict_proba(df_unseen)
+loaded_model.decision_function(df_unseen)
